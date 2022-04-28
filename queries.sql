566d402262426e3172fe693be6be2e34bf605ac7
unknown <sr.skywalking@gmail.com>
2022-04-28 21:55:46 +1000

    Updated clean data and queries

--- queries.sql
+++ queries.sql
@@ -105,7 +105,7 @@ FROM TestInnings20thCentury
 SELECT *
 FROM TestInnings21stCentury;
 
--- Edit our remaining column names and confirm types
+-- Edit our remaining column names and data types
 
 SP_RENAME 'TestInnings20thCentury.Innings Player', 'Player'
 SP_RENAME 'TestInnings21stCentury.Innings Player', 'Player'
@@ -119,65 +119,65 @@ SP_RENAME 'TestInnings20thCentury.Innings Runs Scored Num', 'Runs_Scored'
 SP_RENAME 'TestInnings21stCentury.Innings Runs Scored Num', 'Runs_Scored'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Runs_Scored VARCHAR(50)
+ALTER COLUMN Runs_Scored INT
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Runs_Scored VARCHAR(50)
+ALTER COLUMN Runs_Scored INT
 
 SP_RENAME 'TestInnings20thCentury.Innings Minutes Batted', 'Minutes_Batted'
 SP_RENAME 'TestInnings21stCentury.Innings Minutes Batted', 'Minutes_Batted'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Minutes_Batted VARCHAR(50)
+ALTER COLUMN Minutes_Batted INT
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Minutes_Batted VARCHAR(50)
+ALTER COLUMN Minutes_Batted INT
 
 SP_RENAME 'TestInnings20thCentury.Innings Not Out Flag', 'Not_Out'
 SP_RENAME 'TestInnings21stCentury.Innings Not Out Flag', 'Not_Out'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Not_Out VARCHAR(10)
+ALTER COLUMN Not_Out INT
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Not_Out VARCHAR(10)
+ALTER COLUMN Not_Out INT
 
 SP_RENAME 'TestInnings20thCentury.Innings Balls Faced', 'Balls_Faced'
 SP_RENAME 'TestInnings21stCentury.Innings Balls Faced', 'Balls_Faced'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Balls_Faced VARCHAR(10)
+ALTER COLUMN Balls_Faced INT
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Balls_Faced VARCHAR(10)
+ALTER COLUMN Balls_Faced INT
 
 SP_RENAME 'TestInnings20thCentury.Innings Boundary Fours', 'Fours'
 SP_RENAME 'TestInnings21stCentury.Sixes', 'Fours'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Fours VARCHAR(10)
+ALTER COLUMN Fours INT
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Fours VARCHAR(10)
+ALTER COLUMN Fours INT
 
 SP_RENAME 'TestInnings20thCentury.Innings Boundary Sixes', 'Sixes'
 SP_RENAME 'TestInnings21stCentury.Innings Boundary Sixes', 'Sixes'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Sixes VARCHAR(10)
+ALTER COLUMN Sixes INT
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Sixes VARCHAR(10)
+ALTER COLUMN Sixes INT
 
 SP_RENAME 'TestInnings20thCentury.Innings Batting Strike Rate', 'Batting_Strike_Rate'
 SP_RENAME 'TestInnings21stCentury.Innings Batting Strike Rate', 'Batting_Strike_Rate'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Batting_Strike_Rate VARCHAR(20)
+ALTER COLUMN Batting_Strike_Rate DECIMAL(5,2)
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Batting_Strike_Rate VARCHAR(20)
+ALTER COLUMN Batting_Strike_Rate DECIMAL(5,2)
 
 SP_RENAME 'TestInnings20thCentury.Innings Number', 'Innings_No'
 SP_RENAME 'TestInnings21stCentury.Innings Number', 'Innings_No'
 
 ALTER TABLE CricketStats..TestInnings20thCentury
-ALTER COLUMN Innings_No VARCHAR(10)
+ALTER COLUMN Innings_No INT
 ALTER TABLE CricketStats..TestInnings21stCentury
-ALTER COLUMN Innings_No VARCHAR(10)
+ALTER COLUMN Innings_No INT
 
 SP_RENAME 'TestInnings20thCentury.Innings Date', 'Date'
 SP_RENAME 'TestInnings21stCentury.Innings Date', 'Date'
@@ -194,6 +194,16 @@ ALTER TABLE CricketStats..TestInnings20thCentury
 ALTER COLUMN Runs_Scored_Buckets VARCHAR(20)
 ALTER TABLE CricketStats..TestInnings21stCentury
 ALTER COLUMN Runs_Scored_Buckets VARCHAR(20)
+
+-- Remove apostrophes from column headers
+
+SP_RENAME "TestInnings20thto21stTEST.50's", '50s'
+SP_RENAME "TestInnings20thto21stTEST.[100's]", '100s'
+
+ALTER TABLE CricketStats..TestInnings20thCentury
+ALTER COLUMN [50s] INT
+ALTER TABLE CricketStats..TestInnings21stCentury
+ALTER COLUMN [100s] INT
 
 -- Check data
 
@@ -221,11 +231,10 @@ WHERE Runs_Scored IS NULL
 
 -- Append tables together via Union, sort by Date
 
-SELECT *
+SELECT * INTO TestInnings20thto21st
 FROM TestInnings20thCentury
 UNION
-SELECT *
-FROM TestInnings21stCentury
+SELECT * FROM TestInnings21stCentury
 ORDER BY 'Date';
 
 -- Export from SQL
